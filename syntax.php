5eb8ced19cfafb5f0b6f7293006fc0ea72df0493
mdtareq811 <mdtareq811@gmail.com>
2019-02-06 00:41:06 +0600

    new index.html file create

---
+++ syntax.php
@@ -0,0 +1,59 @@
+<?php
+//    echo "<html>";
+//       echo "<head>";
+//          echo "<title>";
+//              echo "<HOME>";
+//          echo "</title>";
+//       echo "</head>";
+//       echo "<body>";
+//          echo "<h1>This is a heading.</h1>";
+//          echo "<p>This is a paragraph.</p>";
+//        echo "</body>";
+//    echo "</html>";
+?>
+
+
+<!--<html>-->
+<!--     <head>-->
+<!--         <title>--><?php //echo "HOME"; ?><!--</title>-->
+<!--     </head>-->
+<!--     <body>-->
+<!--         <h1>--><?php //echo "This is a heading."; ?><!--</h1>-->
+<!--         <p>--><?php //echo "This is a paragraph."; ?><!--</p>-->
+<!---->
+<!--         <input type="text" value="--><?php //echo "Hello World"; ?><!--"/>-->
+<!--     </body>-->
+<!--</html>-->
+
+<?php
+
+    // 3 major rules for creating a variable
+    /*
+        *Start with $ sign
+        *A-Z, a-z, 0-9
+        *No number in first
+     */
+    // 3 standard rules for creating a variable
+               /*
+                   *small letter
+                   *meaning full
+                   *readable
+                */
+      $NAME;
+      $name;
+      $NaMe;
+      $_9name;
+      $name9;
+
+     // $number = 9;
+//         $firstName = "Rakib";
+//         $lastName = "Murad";
+//         echo $firstName.$lastName;
+
+    /*
+         *Arithmatic Operator
+             *Binary (+,-,*,/,%)
+             *Unary(++,--,(-))
+         *Assignment Operator
+             =, +=, -=, *=, /=, %=, -=
+     */
